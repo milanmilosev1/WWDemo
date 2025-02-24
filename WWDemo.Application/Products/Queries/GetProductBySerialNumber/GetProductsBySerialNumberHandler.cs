17fda4ccc4b87b1b5335b96a0f10f61554a35d16
Milan Milošev <m.milosev-int@levi9.com>
2025-02-24 15:11:39 +0100

    populated GetProductBySerialNumber handler feature/task2

--- WWDemo.Application/Products/Queries/GetProductBySerialNumber/GetProductsBySerialNumberHandler.cs
+++ WWDemo.Application/Products/Queries/GetProductBySerialNumber/GetProductsBySerialNumberHandler.cs
@@ -1,13 +1,26 @@
 ﻿using MediatR;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using WWDemo.Application.DTOs;
+using WWDemo.Data.Products;
 
 namespace WWDemo.Application.Products.Queries.GetProductBySerialNumber
 {
     public class GetProductsBySerialNumberHandler : IRequestHandler<GetProductBySerialNumberQuery, ProductRepresentation>
     {
-        public Task<ProductRepresentation> Handle(GetProductBySerialNumberQuery request, CancellationToken cancellationToken)
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsBySerialNumberHandler(IProductRepository productRepository)
         {
-            throw new NotImplementedException();
+            _productRepository = productRepository;
+        }
+
+        public async Task<ProductRepresentation> Handle(GetProductBySerialNumberQuery request, CancellationToken cancellationToken)
+        {
+            var serialNumber = request.SerialNumber;
+            var product = await _productRepository.GetProductBySerialNumber(serialNumber!);
+            var result = new ProductRepresentation { Category = product?.Category, Name = product?.Name, Price = product?.Price, SerialNumber = product?.SerialNumber };
+
+            return result;
         }
     }
 }
