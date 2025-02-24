b8e4cc0272dcdb0f28eb5305ae99fb21d3fa74de
Milan Milošev <m.milosev-int@levi9.com>
2025-02-24 14:40:28 +0100

    added SerialNumber property in GetProductBySerialNumberQuery feature/task2

--- WWDemo.Api/Requests/AddProductRequest.cs
+++ WWDemo.Api/Requests/AddProductRequest.cs
@@ -1,10 +1,12 @@
-﻿namespace WWDemo.Api.Requests
+﻿using WWDemo.Application.Products.Queries.GetProductBySerialNumber;
+
+namespace WWDemo.Api.Requests
 {
 	public class AddProductRequest
 	{
 		public string? SerialNumber { get; set; }
 		public string? Name { get; set; }
 		public string? Price { get; set; }
-		public string? Category { get; set; }
+		public string? Category { get; set; 
 	}
 }
